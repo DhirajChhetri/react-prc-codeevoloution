ac145ba8ad615d72cc4e7b8d3567de75aa32a632
Dhiraj <dhiraj@yourlink.io>
2019-07-08 09:08:29 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
-
+import Hello from './components/Hello'
 class App extends Component{
  render(){
   return (
     <div className='App'>
       <Greet></Greet>
+      <Hello></Hello>
     </div>
   )
  }
